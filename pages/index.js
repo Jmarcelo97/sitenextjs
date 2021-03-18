42ccea36d11253de0cf8c99774781a2d1a9495d3
João Marcelo <jjmarcelo2@gmail.com>
2021-03-18 11:32:43 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link href="/sobre">
+                <a>Sobre Nós</a>
+            </Link>            
+        </div>
+    );
 }
 
 export default Home;
---
+++ pages/sobre.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <p>Olá! Vejo que você veio nos visitar e nada melhor que uma boa conversa para nos conhecermos melhor. Então, vamos lá! Somos uma empresa nova. Nossas primeiras atividades iniciaram-se em maio de 2019. Somos desenvolvedores apaixonados pelo o que fazemos e dispostos a fazer diferente.</p>
+            <p>Mas, como vamos fazer diferente? Nossa empresa utiliza tecnologias modernas e atuais para o desenvolvimento de aplicativos, websites e sistemas de gestão. Contamos também com processos de tratamentos de dados que são passados para nossos clientes, assim, eles ficam por dentro de tudo que está acontecendo com seu produto. Bom, acho que já falamos muito sobre nós... e gostaríamos de saber um pouco sobre você. Então, entre em contato e nos conte o motivo de sua visita.</p>
+            <Link href="/">
+                <a>Voltar para Home</a>
+            </Link> 
+        </div>
+    );
+}
+
+export default Sobre;
